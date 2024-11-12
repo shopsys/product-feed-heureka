39b3b613834b47e8587c398de8f0843b45e74d08
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-11-12 16:32:32 +0100

    resolved missing typehints reported to be introduced in next package versions

--- src/Form/CategoryFormType.php
+++ src/Form/CategoryFormType.php
@@ -26,7 +26,7 @@ class CategoryFormType extends AbstractType
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param  array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $heurekaCategories = $this->heurekaCategoryFacade->getAllIndexedById();
 
